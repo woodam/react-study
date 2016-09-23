549bd4a6e08914ec8da2eb11792cb80bf56b681e
woodam <woodam@coupang.com>
2016-09-23 16:54:18 +0900

    contact선택 기능 구현

--- src/js/Contact.jsx
+++ src/js/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ContactInfo from './ContectInfo';
+import ContactDetails from './ContactDetails'
 
 export  default class Contact extends React.Component {
     constructor(props){
         super(props);
         this.state = {
+            selectedkey : -1,
             keyword : '',
             contactData : [
                 {name : 'Abet', phone : '010-0000-0001'},
@@ -15,12 +17,21 @@ export  default class Contact extends React.Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(e){
         this.setState({
             keyword : e.target.value
         })
+    }
+
+    handleClick(key){
+        this.setState({
+            selectedkey : key
+        });
+
+        console.log(key, 'is selected');
     }
 
     render(){
@@ -31,9 +42,13 @@ export  default class Contact extends React.Component {
                     return contact.name.toLowerCase().indexOf(this.state.keyword.toLowerCase()) > -1;
                 }
             );
-
+            // 컴포넌트에는 온클릭같은 이벤트가 등록이 안된다 등록하게 되면 props로 전달된다.
+            // 컴포넌트에서 i로 파람을 전달 하면 되지 않는다 그렇지만 arrow 함수로 전달하면 가능하다.
             return data.map((contact,i)=>{
-                return (<ContactInfo contact={contact} key={i}/>);
+                return (<ContactInfo
+                                    contact={contact}
+                                    key={i}
+                                    onClick={ () => this.handleClick(i) } />);
             });
         };
 
@@ -46,7 +61,10 @@ export  default class Contact extends React.Component {
                     value={this.state.keyword}
                     onChange={this.handleChange}
                 />
-                { mapToComponet(this.state.contactData) }
+               <div>{ mapToComponet(this.state.contactData) }</div>
+                <ContactDetails
+                    isSelected={this.state.selectedkey != -1}
+                    contact={this.state.contactData[this.state.selectedkey]}/>
             </div>
         )
     }
--- src/js/contectInfo.jsx
+++ src/js/contectInfo.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
+//컴포넌트component에 이벤트 등록한건 props에 전달 된다.
 export default class ContactInfo extends React.Component {
     render(){
         return(
-            <div>
+            <div onClick={this.props.onClick}>
                 {this.props.contact.name}
-                {this.props.contact.phone}
             </div>
         )
     }
